aae143d0182b5c0c571f7719db611ca6d6eb7e07
Georges Palauqui <g.palauqui@gptechinno.com>
2025-01-17 14:46:25 +0100

    us the PLUG signal as a input to detect HB presence

--- bm13xx-chain/examples/bitcrane-s19jpro.rs
+++ bm13xx-chain/examples/bitcrane-s19jpro.rs
@@ -3,11 +3,11 @@
 use bm1366::BM1366;
 use bm13xx_chain::Chain;
 
-use embedded_hal::digital::{ErrorType, OutputPin};
+use embedded_hal::digital::{ErrorType, InputPin, OutputPin};
 use embedded_hal_async::delay::DelayNs;
 use embedded_hal_bus::i2c::MutexDevice;
 use inquire::Select;
-use std::{env, error::Error, sync::Mutex, time::Duration};
+use std::{error::Error, sync::Mutex, time::Duration};
 use tokio::time::sleep;
 use tokio_adapter::FromTokio;
 use tokio_serial::SerialStream;
@@ -39,27 +39,11 @@ impl OutputPin for FakeOutputPin {
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let mut args: Vec<String> = env::args().collect();
-
-    // use the first arg as serial port, query interactively if not given
-    let hb0_port = if args.len() == 2 {
-        args.pop().unwrap()
-    } else {
-        let ports = tokio_serial::available_ports()?;
-        let ports: Vec<String> = ports.into_iter().map(|p| p.port_name).collect();
-        Select::new("Which serial port should be used?", ports).prompt()?
-    };
-
-    let hb0_builder = tokio_serial::new(hb0_port, 115_200).timeout(Duration::from_millis(50));
-    let hb0_serial = SerialStream::open(&hb0_builder)?;
-    let hb0_uart = FromTokio::new(hb0_serial);
-
-    let bm1362 = BM1366::default(); // TODO: change it to BM1362 when implemented
-
-    // Open FTDI FT4232H channel A from Bitcrane for APW connection
+    // Open FTDI FT4232H channel A from Bitcrane for APW and I2C connection
     let channel_a = ftdi::find_by_vid_pid(0x0403, 0x6011)
         .interface(ftdi::Interface::A)
-        .open()?;
+        .open()
+        .expect("Failed to open FTDI FT4232H channel A");
     let hal_ch_a = ftdi_embedded_hal::FtHal::init_freq(channel_a, 400_000)?;
     let i2c = hal_ch_a.i2c()?;
     let _psu_en = hal_ch_a.ad4()?;
@@ -69,37 +53,52 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let _emc2305_i2c = MutexDevice::new(&Mutex::new(&i2c));
     // TODO: use a emc2305 device driver using emc2305_i2c i2c@0x4d
 
+    let bm1362 = BM1366::default(); // TODO: change it to BM1362 when implemented
+
+    // Only use HB0 for now
     // Open FTDI FT4232H channel B from Bitcrane for HB0 connection
     let channel_b = ftdi::find_by_vid_pid(0x0403, 0x6011)
         .interface(ftdi::Interface::B)
-        .open()?;
+        .open()
+        .expect("Failed to open FTDI FT4232H channel B");
     let hal_ch_b = ftdi_embedded_hal::FtHal::init_default(channel_b)?;
+    let mut hb0_plug0 = hal_ch_b.adi3()?;
+    if hb0_plug0.is_high().unwrap() {
+        let hb0_busy = FakeOutputPin;
+        let hb0_reset = hal_ch_b.ad2()?;
+        let _hb0_pic_i2c = MutexDevice::new(&Mutex::new(&i2c));
+        // TODO: use a pic1704 device driver using hb0_pic_i2c i2c@32
+        let _hb0_temp_a_i2c = MutexDevice::new(&Mutex::new(&i2c));
+        // TODO: use a lm75a device driver using hb0_temp_a_i2c i2c@72
+        let _hb0_temp_b_i2c = MutexDevice::new(&Mutex::new(&i2c));
+        // TODO: use a lm75a device driver using hb0_temp_b_i2c i2c@76
 
-    // Only use HB0 for now
-    let hb0_busy = FakeOutputPin;
-    let hb0_reset = hal_ch_b.ad2()?;
-    let _hb0_plug0 = hal_ch_b.ad3()?;
-    let _hb0_pic_i2c = MutexDevice::new(&Mutex::new(&i2c));
-    // TODO: use a pic1704 device driver using hb0_pic_i2c i2c@32
-    let _hb0_temp_a_i2c = MutexDevice::new(&Mutex::new(&i2c));
-    // TODO: use a lm75a device driver using hb0_temp_a_i2c i2c@72
-    let _hb0_temp_b_i2c = MutexDevice::new(&Mutex::new(&i2c));
-    // TODO: use a lm75a device driver using hb0_temp_b_i2c i2c@76
+        // query interactively the serial port
+        // TODO: try to get it from the FTDI device
+        let hb0_port = {
+            let ports = tokio_serial::available_ports()?;
+            let ports: Vec<String> = ports.into_iter().map(|p| p.port_name).collect();
+            Select::new("Which serial port should be used?", ports).prompt()?
+        };
+        let hb0_builder = tokio_serial::new(hb0_port, 115_200).timeout(Duration::from_millis(50));
+        let hb0_serial = SerialStream::open(&hb0_builder)?;
+        let hb0_uart = FromTokio::new(hb0_serial);
 
-    // TODO: declare HB1 and HB2
-
-    let mut chain0 = Chain::new(126, bm1362, 42, hb0_uart, hb0_busy, hb0_reset, Delay);
-    chain0.enumerate().await?;
-    println!("Enumerated {} asics", chain0.asic_cnt);
-    println!("Interval: {}", chain0.asic_addr_interval);
-    chain0.init(256).await?;
-    chain0.change_baudrate(1_000_000).await?;
-    // chain0.enumerate().await?; // just to be sure the new baudrate is well setup
-    // println!("Enumerated {} asics", chain0.asic_cnt);
-    // println!("Interval: {}", chain0.asic_addr_interval);
-    chain0.reset_all_cores().await?;
-    // chain0.set_hash_freq(HertzU64::MHz(525)).await?;
-    // chain0.enable_version_rolling(0x1fff_e000).await?;
+        let mut chain0 = Chain::new(126, bm1362, 42, hb0_uart, hb0_busy, hb0_reset, Delay);
+        chain0.enumerate().await?;
+        println!("Enumerated {} asics", chain0.asic_cnt);
+        println!("Interval: {}", chain0.asic_addr_interval);
+        chain0.init(256).await?;
+        chain0.change_baudrate(1_000_000).await?;
+        // chain0.enumerate().await?; // just to be sure the new baudrate is well setup
+        // println!("Enumerated {} asics", chain0.asic_cnt);
+        // println!("Interval: {}", chain0.asic_addr_interval);
+        chain0.reset_all_cores().await?;
+        // chain0.set_hash_freq(HertzU64::MHz(525)).await?;
+        // chain0.enable_version_rolling(0x1fff_e000).await?;
+    } else {
+        println!("HB0 is not connected");
+    }
     Ok(())
 }
 
